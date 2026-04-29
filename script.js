aad5c1299260b25ad0a2a5276bf6be84c074edbe
idil-abdi <edilabdiaziz@gmail.com>
2026-04-29 18:16:25 +0100

    created a working game.

--- script.js
+++ script.js
@@ -1,53 +1,140 @@
 const cards = document.querySelectorAll(".inner");
+const score = document.querySelector('#score')
+const highestScore = document.querySelector('#high')
+const lives = document.querySelector('#lives')
+const gameRounds = document.querySelector('#round')
+
 const backEl = document.querySelectorAll('.back')
 
-const stitchImage = document.createElement('img')
-    stitchImage.src = './img/EmojiStitch1.png'
-    stitchImage.classList = 'stitchsize'
+// create an object variable to keep track of my state. 
+const game = {
+    currentScore: 0,
+    highScore: 0,
+    stitchPosition: null,
+    attemptLeft: 3,
+    isRoundActive: false,
+    maxAttempt: 3,
+    rounds: 1,
+}
 
-// defeine game state
-const stitchPostion = Math.floor(Math.random() * 9);
+
+// this function is start the game.
+const initialiseGame = () => {
+    // reset the state variable
+    game.currentScore = 0,
+    game.highScore = 0,
+    game.attemptLeft  = game.maxAttempt,
+    game.isRoundActive = false,
+    game.round = 1
+    
+    updateScoreDisplay()
+    updateAttemptsDisplay()
+    
+    startRound()
+}
+
+const startRound = () => {
+    game.attemptLeft = game.maxAttempt
+    game.stitchPosition = Math.floor(Math.random() * 9)
+    game.isRoundActive = true
+    
+    updateAttemptsDisplay()
+    placeStitch()
+    
+    console.log('Stitch at: ', game.stitchPosition);
+    
+}
 
 cards.forEach((card, index) => {
     card.addEventListener("click", (e) => {
         card.style.transform = "rotateY(180deg)";
-
+        
         setTimeout(() => {
             card.style.transform = "rotateY(0deg)";
         }, 1000);
-
-        if (stitchPostion == index) {
-            console.log(`found stitch in box number ${stitchPostion}`);
-        } else {
-            console.log(`BOO, Stitch is not in card num ${index}`);
-        }
+        
+        handleCardClick(index);
     });
 });
 
-// const findStitch = () => {
-//     const backEl = document.querySelectorAll('.back')
-//     const stitchImage = document.createElement('img')
-//     stitchImage.src = './img/EmojiStitch1.png'
-//     stitchImage.classList = 'stitchsize'
+const placeStitch = () => {
+    backEl.forEach((card) => {
+        card.innerHTML = ''
+    })
+
+    const stitchImage = document.createElement('img')
+    stitchImage.src = './img/EmojiStitch1.png'
+    stitchImage.classList.add('stitchsize')
+
+    backEl[game.stitchPosition].appendChild(stitchImage)
+
+}
+
+const handleCardClick = (index) => {
+    if (!game.isRoundActive) {
+        return
+    }
+    
+    if (game.stitchPosition === index){
+        game.currentScore += game.attemptLeft
+        game.round++;
+        updateRoundDisplay()
+        updateScoreDisplay()
+        endRound()
+        console.log('correct');
+    } else {
+        console.log('wrong');
+        game.attemptLeft--
+        
+        updateAttemptsDisplay()
+        if (game.attemptLeft === 0) {
+            console.log('game over');
+            game.round=1
+            updateRoundDisplay()
+            endRound('lose')
+        }
+    }
+}
+
+const updateScoreDisplay = () => {
+    score.innerHTML = game.currentScore
+}
+
+const updateHighScoreDisplay = () => {
+    highestScore.innerHTML = game.highScore
+}
+const updateAttemptsDisplay = () => {
+    lives.innerHTML = game.attemptLeft
+}
+const updateRoundDisplay = () => {
+    gameRounds.innerHTML = game.round
+}
+
+const endRound = (result) => {
+    game.isRoundActive = false
+    console.log('Round Over');
+
+    if (result === 'lose') {
+        game.currentScore = 0
+        updateScoreDisplay()
+    }
+
+    if (game.currentScore > game.highScore) {
+        game.highScore = game.currentScore;
+        updateHighScoreDisplay()
+    }    
 
     
-//     return 
-        
-// }
+    // cards[game.stitchPosition].style.transform = "rotateY(180deg)";
+    
+    setTimeout(() => {
+        startRound()
+        // cards[game.stitchPosition].style.transform = "rotateY(0deg)";
+    }, 2000);
+    
+}
 
-// console game:
-const currentScore = 0;
-const highScore = 0;
-const stitchLocation = Math.floor(Math.random() * 9);
-const attemptsLeft = 0;
-const isRoundActive = 0;
+document.addEventListener('DOMContentLoaded', (e) => {
+    initialiseGame()
+})
 
-// const gameCard = [0,1,2,3,4,5,6,7,8]
-
-//     if (stitchLocation == gameCardPosition) {
-//         console.log(`found stitch in box number ${stitchLocation}`);
-//     } else {
-//         console.log(`BOO, Stitch is not in card num `);
-//     }
-
-// const userInput =prompt('enter a num from 1 to 9')
